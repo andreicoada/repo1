10f674b715fb73dd5b40415792ce639e249184d1
andreicoada <andreicoada1301@gmail.com>
2020-10-11 14:18:10 +0300

    Delete count.py

--- count.py
+++
@@ -1,9 +0,0 @@
-# Python Program to Count Number of Digits in a Number using While loop
-
-Number = (input("Please Enter any Number: ")
-Count = len()
-while(Number > 0):
-    Number = Number // 10
-    Count = Count + 1
-
-print("\n Number of Digits in a Given Number = %d" %len())
