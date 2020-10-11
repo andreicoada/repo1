5193faa11b2ff8bf5d5ca024ef128f45c321eaee
Andrei <andreicoada1301@gmail.com>
2020-10-11 14:30:00 +0300

    tema_1_si_tema_2

--- temaCurs1_2.py
+++ temaCurs1_2.py
@@ -1,27 +1,28 @@
 # Problema 1
-print("Problema 1, hit Enter to continue")
+print("Problema 1")
+problema = input("\n\tApasa <Enter> pentru a continua")
 brands = ('Asus', 'Hp', 'Lenovo', 'Apple', 'Samsung')
 for a in brands:
-    print(a,'are un numar de',len(a),'caractere')
+    print("\n",a,'are un numar de',len(a),'caractere')
 
 # Problema 2
-
+print("\nProblema 2")
+problema = input("\n\tApasa <Enter> pentru a continua")
 print("\n\tAcum vom valida o adresa de mail.")
 mail ="andrei1301"
-user = input(str("Introdu te rugam o adresa de mail: "))
+user = input(str("\nIntrodu te rugam o adresa de mail: "))
 if user == "andrei1301":
-    print("Felicitari, adresa de mail este valida. Ea este: ",mail,"@gmail.com")
+    print("\nFelicitari, adresa de mail este valida. Ea este: ",mail,"@gmail.com")
 else:
-    print("Ne pare rau dar adresa",user,"@gmail.com nu este corecta")
+    print("\nNe pare rau dar adresa",user,"@gmail.com nu este corecta")
 
 #Problema 3
-
-print("Problema 3. Apasa <Enter> pentru a continua")
-enter = input()
+print("\n\tProblema 3. Apasa <Enter> pentru a continua")
+problema = input()
 numarTel = "0744602279"  #presupunem ca este un nr de tel dintr-o baza de date
 numarTel=input("Adauga te rog nr de tel: ")
 if numarTel == "0744602279":
-    print("Felicitari, ai validat numarul de tel ",numarTel)
+    print("Felicitari, ai validat cu succes numarul de telefon: ",numarTel)
 else:
-    print("Ne pare rau, numarul de teleton ",numarTel, "nu se afla in baza noastra de date")
+    print("Ne pare rau, numarul de teleton ",numarTel, "nu se afla in baza noastra de date!")
 
